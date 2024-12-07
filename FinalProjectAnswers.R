df43f20f94260a3bc3acd154584e104007ee8cbf
kmblincow <kmblincow@gmail.com>
2024-12-07 11:33:51 -0400

    update with google colab tutorials

--- FinalProjectAnswers.R
+++ FinalProjectAnswers.R
@@ -16,6 +16,8 @@ genre <- d %>% select(genre1, genre2, genre3) %>%
   arrange(desc(n)) %>% 
   drop_na()
 
+head(genre, 10)
+
 
 genre2 <- d %>% select(genre1, genre2, genre3) %>% 
   pivot_longer(cols = 1:3, names_to = "genre number")
@@ -44,3 +46,4 @@ q2$month <- month(q2$date_added)
 
 ggplot(data = q2, aes(x = as.factor(month))) +
   geom_bar()
+
